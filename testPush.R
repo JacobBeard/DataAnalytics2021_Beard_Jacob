722cfce503a855a874cd1b8852dc77565df8e588
Jacob Beard <jacobbeard@MacBook-Air.local>
2021-02-18 10:20:07 -0500

    NEWEST

--- testPush.R
+++ testPush.R
@@ -3,3 +3,16 @@ fivenum(data1$EPI,na.rm=TRUE)
 head(data3$EPI)
 
 print("test")
+
+#read.csv("/$home/desktop/multivariate.csv")
+multivariate <- read.csv(file.choose(),header=T)
+attach(multivariate)
+names(multivariate)
+View(multivariate)
+mm <- lm(Homeowners ~ Immigrant)
+mm
+plot(Immigrant,Homeowners)
+HP<- Homeowners/Population
+PD<-Population/area
+mm<-lm(Immigrants~Income+Population+HP+PD)
+Imm <- lm(Immigrants)
