c1cbc50b79916f02bdb54bb051f7213243e3badb
Jacob Beard <jacobbeard@MacBook-Air.local>
2021-03-11 15:22:57 -0500

    NEWEST

---
+++ Lab5.R
@@ -0,0 +1,45 @@
+install.packages("titanic")
+library(titanic)
+
+library(rpart)
+require(rpart)
+Swiss_rpart <- rpart(Fertility ~ Agriculture + Education + Catholic, data = swiss)
+?rpart
+plot(Swiss_rpart) # try some different plot options
+text(Swiss_rpart) # try some different text options
+?text
+Swiss_rpart2 <- rpart(Fertility ~  Education + Catholic, data = swiss)
+plot(Swiss_rpart) # try some different plot options
+text(Swiss_rpart) # try some different text options
+require(party)
+
+treeSwiss<-ctree(Species ~ ., data=iris)
+plot(treeSwiss)
+
+cforest(Species ~ ., data=iris, controls=cforest_control(mtry=2, mincriterion=0))
+
+treeFert<-ctree(Fertility ~ Agriculture + Education + Catholic, data = swiss)
+
+cforest(Fertility ~ Agriculture + Education + Catholic, data = swiss, controls=cforest_control(mtry=2, mincriterion=0))
+# look at help info, vary parameters.
+
+library(tree)
+tr <- tree(Species ~ ., data=iris)
+tr
+tr$frame
+plot(tr)
+text(tr)
+#find "prettier" ways to plot the tree
+
+# Conditional Inference Tree for Mileage
+fit2M <- ctree(Mileage~Price + Country + Reliability + Type, data=na.omit(cu.summary))
+summary(fit2M)
+# plot tree
+plot(fit2M, uniform=TRUE, main="CI Tree Tree for Mileage ")
+text(fit2M, use.n=TRUE, all=TRUE, cex=.8)
+
+fitK <- ctree(Kyphosis ~ Age + Number + Start, data=kyphosis)
+plot(fitK, main="Conditional Inference Tree for Kyphosisâ€)
+plot(fitK, main="Conditional Inference Tree for Kyphosis",type="simple")
+
+#etc.
